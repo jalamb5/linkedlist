91d4fde26d03443e678ac8c228aa4d3803a16035
Justin Lamb <justinlamb@me.com>
2022-04-26 11:17:22 -0700

    append & prepend implemented

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,22 +1,43 @@
 class LinkedList
-  attr_accessor :list
+  attr_accessor :head, :tail
+
   def initialize
-    @list = []
+    @head = nil
+    @tail = nil
   end
 
   def append(value)
-    list << value
-    p list
+    if @head.nil?
+      @head, @tail = value, value
+    else
+      Node.new.value(value)
+      @tail = value
+    end
+  end
+
+  def prepend(value)
+    if @head.nil?
+      @head, @tail = value, value
+    else
+      Node.new.value(value, @head)
+      @head = value
+    end
   end
 end
 
 class Node
+  attr_accessor :data, :next_node
+
   def value(data = nil, next_node = nil)
     @data = data
     @next_node = next_node
   end
 end
 
-myList = LinkedList.new
-myList.append(4)
-myList.append(5)
+list = LinkedList.new
+list.append(454)
+list.append(33)
+p list
+list.prepend(5)
+list.prepend(25)
+p list
